455e78c7ae2b600268f46cb879dc2f9f87cc5a68
Durn02 <engus9902@gmail.com>
2024-04-18 16:40:12 +0900

    ftr: white canvas에서 update 구현

--- src/main/java/canvas/view/WhiteCanvas.java
+++ src/main/java/canvas/view/WhiteCanvas.java
@@ -13,6 +13,9 @@ public class WhiteCanvas extends JPanel implements Observer {
     public WhiteCanvas(Controller controller) {
         this.controller = controller;
 
+//        FlowLayout layout = new FlowLayout(FlowLayout.CENTER);
+//        setLayout(layout);
+
         setBackground(Color.white);
         setVisible(true);
     }
@@ -23,7 +26,20 @@ public class WhiteCanvas extends JPanel implements Observer {
         // shapes가 들어오면 shapes안에 도형이 몇개 있는지 당연히 알 수 있음.
         // 그거 갖고 id 넣으면 됨. (예를 들어 2개 있으면 다음 생성시 3번으로)
         // 일단 현재는 z-order를 id 값과 같은 값으로 넣어두고 나중에 구현
-        System.out.println(shapes.get(0).getShapeType());
-        System.out.println(shapes.size());
+        removeAll();
+        setLayout(null);
+        for (Shape shape : shapes) {
+            JLabel component = new JLabel();
+            component.setText(shape.getShapeType().toString());
+            component.setBounds(shape.getxPos(), shape.getyPos(), shape.getWidth(), shape.getHeight());
+            component.setBackground(shape.getColor());
+
+            add(component);
+
+        }
+//        System.out.println(getComponentCount());
+        revalidate();
+        repaint();
     }
+
 }
