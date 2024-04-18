aaf70a79cdf59061c117aa0588b9ffcaf6c8d6ca
Durn02 <engus9902@gmail.com>
2024-04-18 16:36:02 +0900

    com: add comment

--- src/main/java/canvas/view/Frame.java
+++ src/main/java/canvas/view/Frame.java
@@ -13,21 +13,24 @@ public class Frame extends JFrame{
 
 
     public Frame(Controller controller) {
+        // initiating class
         this.palette = new Palette(controller);
         this.whiteCanvas = new WhiteCanvas(controller);
         this.preferences = new Preferences(controller);
 
+        // register observer to subject
         controller.registerObserver(whiteCanvas);
         controller.registerObserver(preferences);
 
+        // setting frame preferences
         int frame_width = 1300;
         int frame_height = frame_width * 3/5;
 
         setSize(frame_width, frame_height);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         setLayout(new BorderLayout());
 
+        // adding component
         add(new JLabel(" "), BorderLayout.NORTH);
         add(palette, BorderLayout.WEST);
         add(whiteCanvas,BorderLayout.CENTER);
