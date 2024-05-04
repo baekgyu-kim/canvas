7b32318bc9059594b2625a966f4e6c0e2165f8ed
Baekgyu <baekgyu.dev@gmail.com>
2024-05-04 19:40:25 +0900

    rmv: remove util codes

--- src/main/java/canvas/common/utils/ZorderUtils.java
+++
@@ -1,7 +0,0 @@
-package canvas.common.utils;
-
-public class ZorderUtils {
-    public static int convertZorder(int z_order, int shapes_length){
-        return shapes_length - 1 - z_order;
-    }
-}
