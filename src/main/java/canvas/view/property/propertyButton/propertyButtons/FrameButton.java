586917d605d66ac8214d0d88819278dbb9d60456
Baekgyu <baekgyu.dev@gmail.com>
2024-05-14 20:38:04 +0900

    ftr: frame effect 구현 완료

--- src/main/java/canvas/view/property/propertyButton/propertyButtons/FrameButton.java
+++ src/main/java/canvas/view/property/propertyButton/propertyButtons/FrameButton.java
@@ -14,20 +14,47 @@ import java.util.List;
 
 public class FrameButton extends PropertyButton {
     private Boolean hasFrame = null;
+    private JRadioButton activateFrameButton;
+    private JRadioButton deactivateFrameButton;
 
     public FrameButton(ShapeComposite shapeComposite, Controller controller) {
         super(shapeComposite, controller);
-        initializeButton("Change Frame");
+        initializeButton("Frame Effect");
+
+        // 라디오 버튼 초기화
+        activateFrameButton = new JRadioButton("Activate frame effect");
+        deactivateFrameButton = new JRadioButton("Deactivate frame effect");
+
+        // ButtonGroup을 사용하여 두 버튼이 한 그룹에 속하도록 설정
+        ButtonGroup group = new ButtonGroup();
+        group.add(activateFrameButton);
+        group.add(deactivateFrameButton);
+
+        // 버튼에 ActionListener 추가
         addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                int response = JOptionPane.showConfirmDialog(null, "Would you like to add a frame?", "Frame", JOptionPane.YES_NO_OPTION);
-                if (response == JOptionPane.YES_OPTION) {
-                    hasFrame = true;
-                } else if (response == JOptionPane.NO_OPTION) {
-                    hasFrame = false;
+                // 라디오 버튼 패널 생성
+                JPanel panel = new JPanel();
+                panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+                panel.add(activateFrameButton);
+                panel.add(deactivateFrameButton);
+
+                // 라디오 버튼 상태 설정
+                setRadioButtonState();
+
+                // 팝업 창으로 라디오 버튼 패널 표시
+                int result = JOptionPane.showConfirmDialog(null, panel, "Frame", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+
+                // OK 버튼을 누르면 상태 업데이트
+                if (result == JOptionPane.OK_OPTION) {
+                    if (activateFrameButton.isSelected()) {
+                        hasFrame = true;
+                    } else if (deactivateFrameButton.isSelected()) {
+                        hasFrame = false;
+                    }
+                    createPropertyDto(hasFrame);
                 }
-                createPropertyDto(hasFrame);
             }
         });
     }
@@ -49,13 +76,30 @@ public class FrameButton extends PropertyButton {
         }
         if (shapeComposite.shapesCount() == 1) {
             this.hasFrame = shapeComposite.getChildren().get(0).getFrame();
+        } else {
+            this.hasFrame = null;
         }
+        setRadioButtonState();
     }
 
     @Override
     public void updateAllShapes(List<ShapeAbstractClass> shapes) {
         if (shapeComposite != null && shapeComposite.shapesCount() == 1) {
             this.hasFrame = shapeComposite.getChildren().get(0).getFrame();
+        } else {
+            this.hasFrame = null;
+        }
+        setRadioButtonState();
+    }
+
+    private void setRadioButtonState() {
+        if (hasFrame == null) {
+            activateFrameButton.setSelected(false);
+            deactivateFrameButton.setSelected(false);
+        } else if (hasFrame) {
+            activateFrameButton.setSelected(true);
+        } else {
+            deactivateFrameButton.setSelected(true);
         }
     }
 }
