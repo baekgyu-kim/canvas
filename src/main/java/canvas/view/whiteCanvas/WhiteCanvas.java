119442b27457c8953e238782fab9400af73d7498
Baekgyu <baekgyu.dev@gmail.com>
2024-05-14 13:51:28 +0900

    fix: 디버깅 print 코드 삭제

--- src/main/java/canvas/view/whiteCanvas/WhiteCanvas.java
+++ src/main/java/canvas/view/whiteCanvas/WhiteCanvas.java
@@ -71,10 +71,10 @@ public class WhiteCanvas extends JPanel implements Observer {
     @Override
     public void updateClickedShapes(ShapeComposite shapeComposite) {
         this.clickedShapes = shapeComposite.getChildren();
-        System.out.println("========clicked shapes=======");
-        for (ShapeAbstractClass shape : this.clickedShapes) {
-            System.out.println("Shape ID: " + shape.getId());
-        }
+//        System.out.println("========clicked shapes=======");
+//        for (ShapeAbstractClass shape : this.clickedShapes) {
+//            System.out.println("Shape ID: " + shape.getId());
+//        }
         repaint();
     }
 
